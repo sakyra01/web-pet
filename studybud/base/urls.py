1c50efc565a617d6fb4b69fcdc3d1aa1b1b886bc
sakyra01 <glum1419@gmail.com>
2023-01-09 16:08:57 +0300

    add crud

---
+++ studybud/base/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('room/<slug:pk>/', views.room, name="room"),
+
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<slug:pk>', views.updateRoom, name="update-room"),
+    path('delete-room/<slug:pk>', views.deleteRoom, name="delete-room"),
+]
