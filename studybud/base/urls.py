a0f5c4a09b05d4d452593b0b26bdc2c9dc46ba9f
sakyra01 <glum1419@gmail.com>
2023-01-30 19:27:25 +0300

    add chatroom message CRUD

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<slug:pk>', views.updateRoom, name="update-room"),
     path('delete-room/<slug:pk>', views.deleteRoom, name="delete-room"),
+    path('delete-message/<slug:pk>', views.deleteMessage, name="delete-message"),
 ]
