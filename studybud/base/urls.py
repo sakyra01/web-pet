9bb6f8b42b4a3dfd0947f8dd1ab9b44710fb198e
sakyra01 <glum1419@gmail.com>
2023-02-09 01:36:38 +0300

    add mobile adaptivity

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('delete-room/<slug:pk>', views.deleteRoom, name="delete-room"),
     path('delete-message/<slug:pk>', views.deleteMessage, name="delete-message"),
     path('update-user/', views.updateUser, name="update-user"),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
